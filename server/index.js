dfe04e365cd356cef2944e983328cb3a781cc457
hunterZh37 <zzhang978@wisc.edu>
2022-06-14 14:58:07 -0500

    server code

---
+++ server/index.js
@@ -0,0 +1,61 @@
+// const WebSocket = require('ws')
+// const wss = new WebSocket.Server({ port: 8080 },()=>{
+//     console.log('server started')
+// })
+// wss.on('connection', function connection(ws) {
+//    ws.on('message', (data) => {
+//       console.log('data received \n %o',data.toString())
+
+//       ws.send(data);
+//    })
+// })
+// wss.on('listening',()=>{
+//    console.log('listening on 8080')
+// })
+
+//------------------------------------------------------------------------
+
+
+const webSocketsServerPort = 8080;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log('listening on port 8080');
+
+
+const wsServer = new webSocketServer({
+  httpServer: server
+});
+
+const clients = {};
+
+// This code generates unique userid for everyuser.
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+wsServer.on('request', function (request) {
+  var userID = getUniqueID();
+  //console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  //console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ', message.utf8Data);
+
+      // broadcasting message to all connected clients
+      for(key in clients) {
+        clients[key].sendUTF(message.utf8Data);
+        console.log('sent Message to: ', clients[key]);
+      }
+    }
+  })
+});
